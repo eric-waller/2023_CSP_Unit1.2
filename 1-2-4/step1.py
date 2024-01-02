7e3dd5c01eeac01ecc7823f47d7dc8e1ecf1f74e
eric_waller <eric.waller@vtsd.com>
2024-01-02 07:57:45 -0500

    .

--- 1-2-4/step1.py
+++ 1-2-4/step1.py
@@ -1,4 +1,5 @@
 import turtle as trtl
+import random as rand
 
 wn = trtl.Screen()
 wall = trtl.Turtle()
@@ -7,6 +8,8 @@ wall = trtl.Turtle()
 wall_spacing = 20
 wall_color = "peru"
 wall_num = 7
+distance_before_door = 0
+distance_before_wall = 0
 #wall_length = 20
 start_x = 0
 start_y = 0
@@ -25,28 +28,38 @@ def goto_start():
 #Set up maze
 def make_maze(wall_length):
     global wall_spacing
+    global distance_before_door
     for border in range(3):
         wall.penup()
         wall.left(90)
         wall.forward(wall_length+10)
         wall_length += 20
         wall.pendown()
-    for border in range(21):
+    for border in range(22):
         wall.left(90)
-        draw_door()
-        make_wall()
-        wall.forward(wall_length - 10 - (wall_spacing * 2))
+        door_wall_num = rand.randint(1,2)
+        if door_wall_num == 1:
+            draw_door(wall_length)
+            make_wall(wall_length)
+        if door_wall_num == 2:
+            make_wall(wall_length)
+            draw_door(wall_length)
+        wall.forward(wall_length - (wall_spacing * 2) - distance_before_door - distance_before_wall + 40)
         wall_length += 20
 
-def draw_door():
-    wall.forward(10)
+def draw_door(wall_length):
+    global distance_before_door
+    distance_before_door = rand.randint((wall_spacing * 2), wall_length - (wall_spacing * 2))
+    wall.forward(distance_before_door)
     wall.penup()
     wall.forward(wall_spacing * 2)
     wall.pendown()
 
 
-def make_wall():
-    wall.forward(40)
+def make_wall(wall_length):
+    global distance_before_wall
+    distance_before_wall = rand.randint((wall_spacing * 2), wall_length - (wall_spacing * 2))
+    wall.forward(distance_before_wall)
     wall.left(90)
     wall.forward(wall_spacing * 2)
     wall.back(wall_spacing * 2)

c34a68b384fb7d485aa5b7aae8703f161f5918bf
eric_waller <eric.waller@vtsd.com>
2024-01-03 10:06:12 -0500

    .

--- 1-2-4/step1.py
+++ 1-2-4/step1.py
@@ -5,20 +5,20 @@ wn = trtl.Screen()
 wall = trtl.Turtle()
 
 #Wall variables
-wall_spacing = 20
+path_width = 20
 wall_color = "peru"
 wall_num = 7
-distance_before_door = 0
-distance_before_wall = 0
+rainbow_maze_list = ["red","orange red","dark orange","orange","gold","yellow","green yellow","lawn green","spring green","medium spring green","dark turquoise", "royal blue", "medium slate blue", "blue violet", "dark orchid","medium violet red","crimson" ]
+color_num = 0
 #wall_length = 20
 start_x = 0
 start_y = 0
-wall_thick = 5
+wall_thick = 10
 
 #Wall Properties
 wall.pensize(wall_thick)
 wall.speed(0)
-wall.pencolor(wall_color)
+wall.pencolor(rainbow_maze_list[color_num])
 
 def goto_start():
     wall.penup()
@@ -27,43 +27,45 @@ def goto_start():
 
 #Set up maze
 def make_maze(wall_length):
-    global wall_spacing
-    global distance_before_door
-    for border in range(3):
-        wall.penup()
-        wall.left(90)
-        wall.forward(wall_length+10)
-        wall_length += 20
-        wall.pendown()
-    for border in range(22):
-        wall.left(90)
-        door_wall_num = rand.randint(1,2)
-        if door_wall_num == 1:
-            draw_door(wall_length)
-            make_wall(wall_length)
-        if door_wall_num == 2:
-            make_wall(wall_length)
-            draw_door(wall_length)
-        wall.forward(wall_length - (wall_spacing * 2) - distance_before_door - distance_before_wall + 40)
-        wall_length += 20
+    global path_width
+    global color_num
+    for border in range(25):
+        if border < 4:
+            wall.penup()
+            wall.left(90)
+            wall.forward(wall_length+10)
+            wall_length += 20
+            wall.pendown()
+        else:
+            wall.left(90)
+            door_wall_num = rand.randint(1,2)
+            if door_wall_num == 1:
+                draw_door()
+                make_wall()
+            if door_wall_num == 2:
+                make_wall()
+                draw_door()
+            wall.forward(wall_length - 10 - (path_width * 2))
+            wall_length += 20
+            wall.pencolor(rainbow_maze_list[color_num])
+            if color_num > 15:
+                color_num = 0
+            else:
+                color_num += 1
 
-def draw_door(wall_length):
-    global distance_before_door
-    distance_before_door = rand.randint((wall_spacing * 2), wall_length - (wall_spacing * 2))
-    wall.forward(distance_before_door)
+def draw_door():
+    wall.forward(10)
     wall.penup()
-    wall.forward(wall_spacing * 2)
+    wall.forward(path_width * 2)
     wall.pendown()
 
 
-def make_wall(wall_length):
-    global distance_before_wall
-    distance_before_wall = rand.randint((wall_spacing * 2), wall_length - (wall_spacing * 2))
-    wall.forward(distance_before_wall)
+def make_wall():
+    wall.forward(40)
     wall.left(90)
-    wall.forward(wall_spacing * 2)
-    wall.back(wall_spacing * 2)
+    wall.forward(path_width * 2)
+    wall.back(path_width * 2)
     wall.right(90)
 
-make_maze(20)
+make_maze(10)
 wn.mainloop()

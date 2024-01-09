e869703209cb4de5a1e33294a6e73f2ce054588e
eric_waller <eric.waller@vtsd.com>
2024-01-09 07:56:18 -0500

    .

--- 1-2-5/1-2-5.py
+++ 1-2-5/1-2-5.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+import random as rand
 
 wn = Screen()
 wn.bgcolor('green')
@@ -7,19 +8,44 @@ head = Turtle()
 head.color('black')
 head.penup()
 
-speed = 1
+speed = 2
+
+score = 0
+
+apple = Turtle()
+apple.shape("circle")
+apple.color("red")
+
+head_x = head.xcor()
+head_y = head.ycor()
+
+apple_xval = 0
+apple_yval = 0
 
 def travel():
     head.forward(speed)
     wn.ontimer(travel, 10)
 
-wn.onkey(lambda: head.setheading(90), 'Up')
-wn.onkey(lambda: head.setheading(180), 'Left')
-wn.onkey(lambda: head.setheading(0), 'Right')
-wn.onkey(lambda: head.setheading(270), 'Down')
+def apple_move():
+    global apple_xval
+    global apple_yval
+    apple_xval = rand.randint(-200,200)
+    apple_yval = rand.randint(-200,200)
+    apple.goto(apple_xval,apple_yval)
+
+
+if (apple_xval - head_x) < 20:
+    apple_move()
+    score += 1
+
+
+wn.onkey(lambda: head.left(90), 'Left')
+wn.onkey(lambda: head.right(90), 'Right')
+
 
 wn.listen()
 
 travel()
+
 
 wn.mainloop()

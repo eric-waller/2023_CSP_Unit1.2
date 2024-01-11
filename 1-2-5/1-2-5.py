d79c73529d2e52fddfeefdac88ae6ff3dfb6f0d4
eric_waller <eric.waller@vtsd.com>
2024-01-11 09:24:03 -0500

    .

--- 1-2-5/1-2-5.py
+++ 1-2-5/1-2-5.py
@@ -7,37 +7,62 @@ wn.bgcolor('green')
 head = Turtle()
 head.color('black')
 head.penup()
+head.shapesize(2)
 
-speed = 2
+speed = 5
 
-score = 0
+score = -1
 
 apple = Turtle()
 apple.shape("circle")
 apple.color("red")
+apple.penup()
 
-head_x = head.xcor()
-head_y = head.ycor()
+score_write = Turtle()
+score_write.penup()
+score_write.goto(-440,340)
 
-apple_xval = 0
-apple_yval = 0
+
 
 def travel():
+    global score
     head.forward(speed)
+    if abs(head.xcor() - apple.xcor()) < 10:
+        if abs(head.ycor() - apple.ycor()) < 10:
+            move_apple()
+            score += 1
+            score_write.clear()
+            score_write.write(score, font=("Arial", 55, "bold"))
+    if abs(head.xcor()) > 460:
+        game_over()
+    if abs(head.ycor()) > 360:
+        game_over()
     wn.ontimer(travel, 10)
 
-def apple_move():
-    global apple_xval
-    global apple_yval
-    apple_xval = rand.randint(-200,200)
-    apple_yval = rand.randint(-200,200)
-    apple.goto(apple_xval,apple_yval)
+def move_apple():
+    global speed
+    loop = 0
+    apple.goto(rand.randint(-300,300),rand.randint(-300,300))
+    loop += 1
+    if loop == 5:
+        speed += 1
+        loop = 0
 
 
-if (apple_xval - head_x) < 20:
-    apple_move()
-    score += 1
 
+def game_over():
+    global final_message
+    global score
+    apple.hideturtle()
+    head.hideturtle()
+    score_write.clear()
+    score_write.goto(-200,0)
+    score_write.write("GAME OVER", font=("Arial", 55, "bold"))
+    score_write.goto(-200,-55)
+    score_write.write("FINAL SCORE:",font=("Arial", 55, "bold"))
+    score_write.goto(-50, -110)
+    score_write.write(score,font=("Arial", 55, "bold"))
+    head.goto(0,0)
 
 wn.onkey(lambda: head.left(90), 'Left')
 wn.onkey(lambda: head.right(90), 'Right')
@@ -46,6 +71,6 @@ wn.onkey(lambda: head.right(90), 'Right')
 wn.listen()
 
 travel()
-
+move_apple()
 
 wn.mainloop()

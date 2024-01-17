39f54be885fdd1f8f4207021795697c7c82dd594
eric_waller <eric.waller@vtsd.com>
2024-01-17 11:07:07 -0500

    .

--- 1-2-5/1-2-5.py
+++ 1-2-5/1-2-5.py
@@ -12,7 +12,7 @@ head.shapesize(2)
 speed = 5
 
 score = -1
-
+color_list = ["red", "orange", "yellow"]
 apple = Turtle()
 apple.shape("circle")
 apple.color("red")
@@ -26,16 +26,19 @@ score_write.goto(-440,340)
 
 def travel():
     global score
+    global speed
     head.forward(speed)
+    print(speed)
     if abs(head.xcor() - apple.xcor()) < 10:
         if abs(head.ycor() - apple.ycor()) < 10:
             move_apple()
             score += 1
             score_write.clear()
             score_write.write(score, font=("Arial", 55, "bold"))
+            speed += score/4
     if abs(head.xcor()) > 460:
         game_over()
-    if abs(head.ycor()) > 360:
+    if abs(head.ycor()) > 390:
         game_over()
     wn.ontimer(travel, 10)
 
@@ -43,10 +46,12 @@ def move_apple():
     global speed
     loop = 0
     apple.goto(rand.randint(-300,300),rand.randint(-300,300))
+    color_num = rand.randint(0,len(color_list))
+    apple.color(color_list[color_num])
     loop += 1
-    if loop == 5:
-        speed += 1
-        loop = 0
+    if loop == 2:
+        speed += 5
+        loop -= 2
 
 
 
@@ -63,6 +68,14 @@ def game_over():
     score_write.goto(-50, -110)
     score_write.write(score,font=("Arial", 55, "bold"))
     head.goto(0,0)
+
+if score == 1:
+    color_list.append("blue")
+if score == 2:
+    color_list.append("purple")
+if score == 3:
+    color_list.append("white")
+
 
 wn.onkey(lambda: head.left(90), 'Left')
 wn.onkey(lambda: head.right(90), 'Right')
